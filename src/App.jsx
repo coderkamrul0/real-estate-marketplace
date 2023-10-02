ce32cb2ad7e38672c7e792d954b9e8bd20aa2b69
Kamrul Hasan <kamrul444216@gmail.com>
2023-10-02 23:53:29 +0600

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import SingIn from "./pages/Signin/Singin";
+import SignUp from "./pages/SignUp/SignUp";
+import Profile from "./pages/Profile/Profile";
 function App() {
   return (
-    <>
-      <h1 className="text-4xl font-bold text-center">Real Estate</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SingIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/SignIn/SingIn.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const SingIn = () => {
+  return (
+    <div>SingIn</div>
+  )
+}
+
+export default SingIn
