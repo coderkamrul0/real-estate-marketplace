79deed1067967d7cf41d20fafa7b8b91500c2484
Kamrul Hasan <kamrul444216@gmail.com>
2023-10-03 03:47:32 +0600

    create middleware

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRouter from './routes/user.route.js'
-import authRouter from './routes/auth.route.js'
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
 mongoose
@@ -15,12 +15,20 @@ mongoose
   });
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
-
 app.use("/server/user", userRouter);
 app.use("/server/auth", authRouter);
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
