8c802ec73d58e7590f24a40fbbaae255fce198db
Kamrul Hasan <kamrul444216@gmail.com>
2023-10-03 02:55:08 +0600

    test api create

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
+import userRouter from './routes/user.route.js'
 dotenv.config();
 
 mongoose
@@ -10,7 +11,6 @@ mongoose
   })
   .catch((err) => {
     console.log(err);
-    
   });
 
 const app = express();
@@ -18,3 +18,6 @@ const app = express();
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
+
+app.use("/server/user", userRouter)
