5f64acd8f64e913a0f1e533bcdfe1a5d62834343
Kamrul Hasan <kamrul444216@gmail.com>
2023-10-03 23:01:47 +0600

    sign in functionality

--- client/src/pages/SignIn/SingIn.jsx
+++ client/src/pages/SignIn/SingIn.jsx
@@ -1,11 +1,49 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-const SingIn = () => {
+const SignIn = () => {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch("/server/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate("/");
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+
   return (
     <div className="h-[calc(100vh-72px)] w-full flex justify-center items-center bg-slate-300 px-5 md:px-0">
       <form
+        onSubmit={handleSubmit}
         className="bg-white p-8 rounded-lg shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px] w-96"
       >
         <h2 className="text-2xl font-semibold mb-4 text-center">Sign In</h2>
@@ -14,6 +52,7 @@ const SingIn = () => {
           <input
             type="email"
             id="email"
+            onChange={handleChange}
             name="email"
             placeholder="Email"
             className="w-full px-4 py-2 border border-black rounded-lg focus:outline-none "
@@ -24,6 +63,7 @@ const SingIn = () => {
           <input
             type="password"
             id="password"
+            onChange={handleChange}
             name="password"
             placeholder="Password"
             className="w-full px-4 py-2 border border-black rounded-lg focus:outline-none "
@@ -31,15 +71,14 @@ const SingIn = () => {
           />
         </div>
         <div>
-          {/* {error && <small>{error}</small>} */}
+          {error && <small>{error}</small>}
         </div>
         <button
-          // disabled={loading}
+          disabled={loading}
           type="submit"
           className="bg-black text-white px-4 py-2 rounded-lg hover:bg-transparent hover:text-black border border-black transition-all focus:outline-none  w-full"
         >
-          {/* {loading ? "Loading..." : "Sing Up"} */}
-          Sign In
+          {loading ? "Loading..." : "Sing In"}
         </button>
         <div className="text-center mt-4">
           <button className="bg-black text-white px-4 py-2 rounded-lg hover:bg-transparent hover:text-black border border-black transition-all focus:outline-none  w-full">
@@ -48,12 +87,12 @@ const SingIn = () => {
         </div>
         <div className="mt-4 text-center">
           <p>
-            Already have an account? <Link to={"/sign-up"}>Sign up</Link>
+            Dont have an account? <Link to={"/sign-up"}>Sign Up</Link>
           </p>
         </div>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default SingIn
+export default SignIn;
